01646de1d8a8e24ba00df9e48d9919c0404b980f
audrey <audrey.nageotte@etu.univ-lyon1.fr>
2024-03-04 13:52:45 +0100

    Modification du HTML du site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post #RM
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all() #RM
+    return render(request, 'blog/post_list.html', {'posts': posts})
